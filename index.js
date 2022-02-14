5b07a07598e326620da640fa1830d153600c20ea
DeveHk <connect.to.hk@gmail.com>
2022-02-15 00:06:37 +0530

    Portfolio site

---
+++ index.js
@@ -0,0 +1,9 @@
+let triple = document.getElementById("triple");
+let side=document.querySelector(".sidebar")
+triple.addEventListener("click", (e) => {
+    if (side.classList.replace("sidebar", "hideSidebar"));
+    else side.classList.replace("hideSidebar", "sidebar");
+    side.classList.remove("sidebarHid")
+    
+})
+ 
